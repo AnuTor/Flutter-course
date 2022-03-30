ad7ef1f7cd566ec11119850b2204a94d37a72254
Alejo <alejo_g.p@hotmail.com>
2022-03-30 19:50:20 -0300

    Actualizado a lectura 95

--- app2_personal_expense_app/lib/main.dart
+++ app2_personal_expense_app/lib/main.dart
@@ -10,7 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+      ),
       home: MyHomePage(),
     );
   }
@@ -62,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal expenses'),
         actions: [
           IconButton(
             onPressed: () => _startAddNewTransaction(context),
